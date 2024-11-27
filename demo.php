be38904001b25722e5f2aba6d112939528bca655
Hassad674 <h.smara@it-students.fr>
2024-11-27 12:10:05 +0100

    add demo + tableau formation php

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php 
+    // $nickname = 'One Punch Man';
+    // $prenom ='Saitama';
+    // echo $nickname . ' '. $prenom;
+    $nom = 'Doe';
+    $prenom = 'John';
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = (($note1 + $note2) / 2);
+    // echo 'Bonjour' . ' ' . $prenom . ' ' . $nom . ', vous avez eu'. ' ' .(($note1 + $note2) / 2) . ' '. 'de moyenne.'
+    echo "Bonjour $prenom $nom, vous avez eu $moyenne de moyenne";
+?>
