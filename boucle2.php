fc80633037d2b60a7d85f231399a8693ab8fcd7f
Mehdi Essendi <m.essendi@it-students.fr>
2024-11-28 16:17:54 +0100

    add exo 2 boucle

---
+++ boucle2.php
@@ -0,0 +1,4 @@
+<?php
+/* On veut demander à l'utilisateur de rentrer les horaires d'ouverture d'un magasin
+   On demande à l'utilisateur de rentrer une heure et on lui dira si le magasin est ouvert
+*/
