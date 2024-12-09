3834a69302bf33ce8f8a6b3050e2227041c856e2
essendim <m.essendi@it-students.fr>
2024-12-09 16:15:24 +0100

    add function2

--- fonction2.php
+++ fonction2.php
@@ -10,21 +10,46 @@
 // echo bonjour();
 // echo bonjour('jean');
 
-function repondre_oui_non($texte){
-    $reponse = readline($texte . " (o/n) : ");
+// function repondre_oui_non($texte){
+//     $reponse = readline($texte . " (o/n) : ");
 
-    while(true){
-        if( $reponse === 'o'){
-            return true;
-        } elseif($reponse === 'n'){
-            return false;
+//     while(true){
+//         if( $reponse === 'o'){
+//             return true;
+//         } elseif($reponse === 'n'){
+//             return false;
+//             break;
+//         }
+//     }
+// }
+
+// $resultat = repondre_oui_non('voulez-vous continuer');
+
+
+function demander_creneau($phrase = "Veuillez entrer un creneau")
+{
+    $phrase . "\n";
+    while (true) {
+        $heureDebut = (int) readline(prompt: "Veuillez une heure de début :");
+
+        if ($heureDebut >= 0 && $heureDebut <= 23) {
             break;
         }
     }
 
+    while (true) {
+        $heureFin = (int) readline("Veuillez une heure de fin :");
+
+        if ($heureFin >= 0 && $$heureFin <= 23 && $heureFin > $heureDebut) {
+            break;
+        }
+    }
+    return [$heureDebut, $heureFin];
+
 
 }
 
-$resultat = repondre_oui_non('voulez-vous continuer');
+$creneau = demander_creneau();
+$creneau2 = demander_creneau("Veuillez entrer un créneau ?");
 
-var_dump($resultat);
+var_dump($creneau, $creneau2);
